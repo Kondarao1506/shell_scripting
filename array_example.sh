286e97d33cc6525fc61435a934c2c5bf9054b21e
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-26 22:32:38 +0530

    modified

--- array_example.sh
+++ array_example.sh
@@ -4,3 +4,12 @@ echo "First fruit name is : $FRUITS[0]"
 echo "last fruit name is : ${FRUITS[3]}"
 echo "all the fruits names : ${FRUITS[@]}"
 echo "present working directory : $PWD"
+DATE=$(date +%F)
+echo "Today date is $DATE"
+echo "ALL THE VARIABLE USED AT A TIME IN SCRIPT $@"
+echo "number of variables passed : $#"
+echo "script name $o"
+echo "home directory $HOME"
+echo "pid of script executing is : $$ "
+sleep 100 &
+echo "last background command $!"
