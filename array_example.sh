ba9fb0847086d009e0f0f83c4f0c3f60b42b33ba
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-26 22:23:06 +0530

    modified

--- array_example.sh
+++ array_example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FRUITS=( "APPLE" "BANNANA" "GRAPES" "GUOA")
+FRUITS=( "APPLE" "BANNANA" "GRAPES" "GUOA" )
 echo "First fruit name is : $FRUITS[0]"
-echo "last fruit name is : $FRUITS[3]"
-echo "all the fruits names : $FRUITS[@]"
+echo "last fruit name is : ${FRUITS[3]}"
+echo "all the fruits names : ${FRUITS[@]}"
 echo "present working directory : $PWD"
