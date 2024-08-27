d729a0ca79476137e4408cd5f38916cc67c5e78a
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-27 20:39:15 +0530

    installing sql

---
+++ installing_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+USER=$(id -u)
+R=$(\e[31m)
+N=$(\e[0m)
+if[ USER -ne 0]
+then
+    echo -e "$R please run with privilages $N";
+    exit 1;
+else
+    dnf installed mysql-server -y;
+    if[$? -ne 0]
+    than
+        echo "mysql is installing"
+        dnf install mysql-server -y;
+    else
+        echo "my sql server already installed";
+    fi
+fi
