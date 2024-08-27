3f1cf80f59c70eaf7f03fa80177ac5b12f04967d
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-27 23:46:50 +0530

    fjdsjfk

--- checkingrootaccess.sh
+++ checkingrootaccess.sh
@@ -17,34 +17,34 @@ fi
 
 dnf list installed git -y
 
-VALIDATE $? "listing git"
+if [ $? -ne 0 ]
+then
+    echo "installing git"
+    dnf install git -y
+    VALIDATE $? "listing git"
+    # if [ $? -ne 0 ]
+    # then
+    #     echo "git installation not success"
+    # else
+    #     echo "git installation success"
+    # fi
+else
+    echo "Git already installed"
+fi
 
-# if [ $? -ne 0 ]
-# then
-#     echo "installing git"
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "git installation not success"
-#     else
-#         echo "git installation success"
-#     fi
-# else
-#     echo "Git already installed"
-# fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "my sql is going to installed"
+    dnf install mysql-server -y
+    VALIDATE $? "listing mysql"
+    # if [ $? -ne 0 ]
+    # then 
+    #     echo "my sql not installed successfully"
+    # else
+    #     echo "my sql installed succesfully ok"
+    # fi
 
-# dnf list installed mysql
-# if [ $? -ne 0 ]
-# then
-#     echo "my sql is going to installed"
-#     dnf install mysql-server -y
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "my sql not installed successfully"
-#     else
-#         echo "my sql installed succesfully ok"
-#     fi
-
-# else
-#     echo "mysql installed allredy"
-# fi
+else
+    echo "mysql installed allredy"
+fi
