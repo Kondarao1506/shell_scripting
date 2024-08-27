f8aa28c7337de68b80f717c9f55d7a68420815d8
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-27 22:53:38 +0530

    fjdsjfk

--- checkingrootaccess.sh
+++ checkingrootaccess.sh
@@ -5,11 +5,19 @@ then
     echo "run with root privilages"
     exit 1
 fi
+
 dnf list installed git -y
+
 if [ $? -ne 0 ]
 then
     echo "installing git"
-    dnf install git -y
+    dnf install gittt -y
+    if [ $? -ne 0 ]
+    then
+        echo "git installation not success"
+    else
+        echo "git installation success"
+    fi
 else
     echo "already installed"
 fi
