8f41a37c40dbea8d88720b83accee4ed25a14175
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-27 20:58:28 +0530

    fjdsjfk

--- installing_mysql.sh
+++ installing_mysql.sh
@@ -2,13 +2,13 @@
 USER=$(id -u)
 R="\e[31m"
 N="\e[0m"
-if[ $USER -ne 0 ]
+if [ $USER -ne 0 ]
 then
     echo -e "$R please run with privilages $N"
     exit 1
 else
     dnf installed mysql-server -y
-    if[ $? -ne 0 ]
+    if [ $? -ne 0 ]
     then
         echo "mysql is installing"
         dnf install mysql-server -y
