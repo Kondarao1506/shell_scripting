56be6041440e2081fe662a11296ba042555431da
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-08-28 23:29:26 +0530

    shell_scripting

---
+++ redirectors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+LOGS_FOLDER="/var/log/shell_script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+mkdir -p $LOGS_FOLDER
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+ ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R RUN WITH ROOT PRIVILIZES $N"&>>LOG_FILE
+        exit 1
+    else
+        echo -e "$G YOU ARE IN ROOT PRIVILAGES $N"&>>LOG_FILE
+    fi
+    }
+ VALIDATE(){
+    if [ $? -ne 0 ]
+ then
+    echo "$2 is failed to install"&>>LOG_FILE
+ else
+    echo "$2 ALREADY installed succefully"&>>LOG_FILE
+
+ fi
+ }
+ ROOT
+for package in $@
+  do
+    dnf list installed $package -y &>>LOG_FILE
+    if [ $? -ne 0 ]
+    then    
+        echo "$package is installing"
+        dnf install $package -y &>>LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo "$package installed already" &>>LOG_FILE
+    fi
+  done
