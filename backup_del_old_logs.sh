700c216bd7c384cb261ce2d8b2317fac5d9fa4cf
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-09-02 11:58:13 +0530

    logs

---
+++ backup_del_old_logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+SOURCE_DIR=$1
+DEST_FILE=$2
+DAYS=${3:-14}
+
+USAGE{
+echo "you must paas argumets like source file path and destination file path"
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+    exit 1
+fi
+
+if [ !-d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR : not exist"
+fi
+ 
+if [ !-d $DEST_FILE ]
+then
+    echo "$DEST_FILE : not exist"
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+
+if [!-f $FILES]
+then
+    echo "there are no  files found above $DAYS"
+    exit 1
+else
+    echo "backup files are found"
+fi
+
