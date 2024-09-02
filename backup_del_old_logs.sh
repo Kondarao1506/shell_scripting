101899a57ffaa3ac53fcc04c0d8261781047cb8a
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-09-02 14:22:06 +0530

    logs

--- backup_del_old_logs.sh
+++ backup_del_old_logs.sh
@@ -2,7 +2,7 @@
 SOURCE_DIR=$1
 DEST_FILE=$2
 DAYS=${3:-14}
-
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 USAGE(){
 echo "you must paas argumets like source file path and destination file path"
 }
@@ -31,6 +31,15 @@ if [ -z $FILES ]
 then
     echo "there are no files found above $DAYS"
 else
-    echo "there are  files found older than $DAYS"
+    echo "OLDER THAN $DAYS DAYS FILES FOUND "
+    ZIP_FILE=$DEST_FILE/backuplogs-$TIME_STAMP.zip
+    ZIP_FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14) | zip $ZIP_FILE -@
 fi
+while IFS= read -r file
+do
+    echo "files :- $file"
 
+
+
+
+done <<< $ZIP_FILES
