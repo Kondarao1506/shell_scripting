24820798be43f37f034f5f940bd0bfeae06581a6
kondarao056 <43640445+kondarao056@users.noreply.github.com>
2024-09-02 16:07:33 +0530

    logs

--- backup_del_old_logs.sh
+++ backup_del_old_logs.sh
@@ -32,7 +32,7 @@ then
     echo "there are no files found above $DAYS"
 else
     echo "OLDER THAN $DAYS DAYS FILES FOUND "
-    ZIP_FILE="$DEST_FILE/backuplogs-$TIME_STAMP.zip"
+    #ZIP_FILE="$DEST_FILE/backuplogs-$TIME_STAMP.zip"
     find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip $ZIP_FILE -@
     if [ -f $ZIP_FILE ]
     then
